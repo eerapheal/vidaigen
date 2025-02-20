ef809fed88f7c8abc7cdd1dac9052ce0a0898345
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-20 19:01:08 +0530

    header component

---
+++ src/app/_components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center'>
+      <div className='flex items-center'>
+        <Image
+          src={"/logo.svg"}
+          alt='logo image'
+          width={40}
+          height={40}
+        />
+        <h2 className='text-3xl font-bold text-neutral-50'>VIDAIGEN</h2>
+      </div>
+      <div>
+        <Button className="text-lg font-medium p-5 bg-blue-800">Get Started</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
