56b1cbf14e0b2eab5f859c437871238551dae269
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-23 12:39:32 +0530

    user session only

--- src/app/(main)/provider.jsx
+++ src/app/(main)/provider.jsx
@@ -1,8 +1,26 @@
+"use client"
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "./_component/AppSidebar";
 import HeaderApp from "./_component/HeaderApp";
+import { useAuthContext } from "../Provider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function DashboardProvider({ children }) {
+ 
+  const {user} = useAuthContext();
+  const route = useRouter();
+
+  useEffect(() => {
+  user && CheckedUserAuthenticated()
+  },[!user]);
+
+  const CheckedUserAuthenticated = () => {
+    if (!user) {
+      route.replace("/");
+    }
+    return null;
+  }
   return (
     <div>
       <SidebarProvider>
