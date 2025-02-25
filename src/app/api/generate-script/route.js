7e785c0fdc01d5a580370fa3425d9c49ae06b213
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-25 14:49:39 +0530

    generate scripts api

---
+++ src/app/api/generate-script/route.js
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { chatSession } from "../../../../config/AiModels";
+
+const SCRIPT_PROMPT = `
+write a two deferent script for 30 seconds video on the topic: {topic}
+
+do not add Scene description
+
+Give me a response in JSON format and follow the schema
+
+{scripts: [
+{
+content: ""
+}
+],
+},`;
+
+export async function POST(req) {
+  try {
+    const { topic } = await req.json();
+    if (!topic) {
+      return NextResponse.json({ error: "Topic is required" }, { status: 400 });
+    }
+
+    const PROMPT = SCRIPT_PROMPT.replace("{topic}", topic);
+
+    // Send the prompt to the chat session
+    const result = await chatSession.sendMessage(PROMPT);
+    const resp = result?.response?.text();
+
+    // Parse the response and return it
+    try {
+      const parsedResponse = JSON.parse(resp);
+      return NextResponse.json(parsedResponse);
+    } catch (error) {
+      console.error("Error parsing JSON response:", error);
+      return NextResponse.json(
+        { error: "Invalid JSON response" },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    console.error("Error generating script:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
