d7c349f781be1bd765c8a5c66f87d0fbd71149de
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-26 15:38:08 +0530

    scrollbar

--- src/app/(main)/create-new-video/page.jsx
+++ src/app/(main)/create-new-video/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Topic from './_components/Topic';
 import VideoStyles from './_components/VideoStyles';
+import VoiceOver from './_components/VoiceOver';
 
 const CreateNewVideo = () => {
 
@@ -18,24 +19,21 @@ const CreateNewVideo = () => {
     <div>
       <h2 className="">Create New Video</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 mt-8">
-        <div className="col-span-2 p-7 rounded-lg border">
+        <div className="col-span-2 p-7 rounded-lg border h-[79vh] overflow-auto hide-scrollbar">
           {/* Topic */}
           <Topic onHandleInputChange={onHandleInputChange} />
           {/* Video Images */}
-          <VideoStyles onHandleInputChange={onHandleInputChange}/>
+          <VideoStyles onHandleInputChange={onHandleInputChange} />
           {/* Voice over */}
-
+          <VoiceOver />
           {/* Cations */}
         </div>
         <div className="col-span-1">
           {/* creacted vudeo */}
-          
         </div>
       </div>
-
     </div>
   );
 };
 
 export default CreateNewVideo;
-
