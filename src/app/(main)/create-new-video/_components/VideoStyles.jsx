041b1b48cf3f5a54b67a42087acbd54688fc2b0c
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-26 11:38:06 +0530

    video style UI/UX

--- src/app/(main)/create-new-video/_components/VideoStyles.jsx
+++ src/app/(main)/create-new-video/_components/VideoStyles.jsx
@@ -1,9 +1,78 @@
+import Image from 'next/image';
 import React from 'react'
 
-const VideoStyles = ({onHandleInputChange}) => {
+const options = [
+  {
+    name: 'Realistic',
+    image: '/hummingbird.jpg'
+  },
+  {
+    name: 'Artwork',
+    image: '/from.jpg'
+  },
+  {
+    name: 'Cartoon',
+    image: '/loeo.jpg'
+  },
+  {
+    name: 'Cinematic',
+    image: '/darkstre.jpg'
+  },
+  {
+    name: 'Hygiene',
+    image: '/Hygiene.jpg'
+  },
+  {
+    name: 'Super',
+    image: '/megayak.jpg'
+  },
+  {
+    name: 'Elderly',
+    image: '/elderly_African.jpg'
+  },
+  {
+    name: 'History',
+    image: '/mome.jpg'
+  },
+  {
+    name: 'Horously',
+    image: '/pulse.jpg'
+  },
+  {
+    name: 'Waterfall',
+    image: '/perspective.jpg'
+  },
+  {
+    name: 'Reality',
+    image: '/Reality.jpg'
+  },
+  {
+    name: 'Pirate',
+    image: '/some_1.jpg'
+  },
+];
+
+const VideoStyles = ({ onHandleInputChange }) => {
   return (
     <div>
-      VideoStyles
+      <h2 className='mt-3'>Video Styles</h2>
+      <p className='m-1'>Select Video Style</p>
+      <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-2'>
+        {
+          options.map((option, index) => (
+            <div key={index} className='relative'>
+              <Image
+                src={option.image}
+                alt={option.name}
+                width={500}
+                height={120}
+                className='object-cover rounded shadow-2xl cursor-pointer h-[90px] lg:h-[120px] xl-h-[180px] hover:border border-slate-400'
+              />
+              <h3 className='absolute bottom-1 text-center w-full text-slate-50'>{option.name}</h3>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
