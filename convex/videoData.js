497b9c722cf96807aac67d6845fc0011a4b82bdf
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-02-28 15:55:37 +0530

    create video data api

---
+++ convex/videoData.js
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const CreateVideoData = mutation({
+  args: {
+    title: v.string(),
+    topic: v.string(),
+    script: v.string(),
+    videoStyle: v.string(),
+    voice: v.string(),
+    caption: v.any(),
+    uid: v.id("users"),
+    createdBy: v.string(), // Ensure this is included in the args
+  },
+
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("videoData", {
+      title: args.title,
+      topic: args.topic,
+      script: args.script,
+      videoStyle: args.videoStyle,
+      voice: args.voice,
+      caption: args.caption,
+      uid: args.uid,
+      createdBy: args.createdBy,
+    });
+    return result;
+  },
+});
