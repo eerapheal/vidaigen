fb1db55349996393d8d7d6bcf4a1923f4b0ad03e
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-03-01 07:18:47 +0530

    complete video generation steps with inngest

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -40,96 +40,96 @@ export const GenerateVideoData = inngest.createFunction(
     const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL);
     // generate mp3 file
     const GenerateAudioFile = await step.run("GenerateAudioFile", async () => {
-      // const result = await axios.post(
-      //   BASE_URL + "/api/text-to-speech",
-      //   {
-      //     input: script,
-      //     voice: voice,
-      //   },
-      //   {
-      //     headers: {
-      //       "x-api-key": process.env.NEXT_PUBLIC_AIGURULAB_API_KEY,
-      //       "Content-Type": "application/json",
-      //     },
-      //   }
-      // );
-      // console.log(result.data.audio);
-      return "https://firebasestorage.googleapis.com/v0/b/projects-2025-71366.firebasestorage.app/o/audio%2F1740764351638.mp3?alt=media&token=59102e81-b52e-48ca-9878-fef6be355d65";
+      const result = await axios.post(
+        BASE_URL + "/api/text-to-speech",
+        {
+          input: script,
+          voice: voice,
+        },
+        {
+          headers: {
+            "x-api-key": process.env.NEXT_PUBLIC_AIGURULAB_API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.error(result.data.audio);
+      return result.data.audio;
     });
 
     // generate audio speech to text
 
-    //   const GenerateCaption = await step.run("GenerateCaption", async () => {
-    //     // STEP 1: Create a Deepgram client using the API key
-    //     const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY);
-    //     // STEP 2: Call the transcribeUrl method with the audio payload and options
-    //     const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
-    //       {
-    //         url: GenerateAudioFile,
-    //       },
-    //       // STEP 3: Configure Deepgram options for audio analysis
-    //       {
-    //         model: "nova-3"
-    //       }
-    //     );
+    const GenerateCaption = await step.run("GenerateCaption", async () => {
+      // STEP 1: Create a Deepgram client using the API key
+      const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY);
+      // STEP 2: Call the transcribeUrl method with the audio payload and options
+      const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+        {
+          url: GenerateAudioFile,
+        },
+        // STEP 3: Configure Deepgram options for audio analysis
+        {
+          model: "nova-3",
+        }
+      );
 
-    //     if (error) throw error;
-    // // STEP 4: Print the results
+      if (error) throw error;
+      // STEP 4: Print the results
 
-    //     return result.results?.channels[0]?.alternatives[0]?.words;
-    //   });
+      return result.results?.channels[0]?.alternatives[0]?.words;
+    });
 
     // generate prompts for image
-    // const GenerateImagePrompt = await step.run(
-    //   "GenerateImagePrompt",
-    //   async () => {
-    //     const FINAL_PROMPT = ImagePromptScript.replace(
-    //       "{style}",
-    //       videoStyle
-    //     ).replace("script", script);
-    //     const result = await GenerateImageScript.sendMessage(FINAL_PROMPT);
-    //     const resp = JSON.parse(result.response.text());
-    //     return resp;
-    //   }
-    // );
+    const GenerateImagePrompt = await step.run(
+      "GenerateImagePrompt",
+      async () => {
+        const FINAL_PROMPT = ImagePromptScript.replace(
+          "{style}",
+          videoStyle
+        ).replace("script", script);
+        const result = await GenerateImageScript.sendMessage(FINAL_PROMPT);
+        const resp = JSON.parse(result.response.text());
+        return resp;
+      }
+    );
     //  generate image using A
-    // const GenerateImage = await step.run("generateImage", async () => {
-    //   let images = [];
-    //   images = await Promise.all(
-    //     GenerateImagePrompt.map(async (element) => {
-    //       const result = await axios.post(
-    //         BASE_URL + "/api/generate-image",
-    //         {
-    //           width: 1024,
-    //           height: 1024,
-    //           input: element.imagePrompt,
-    //           model: "sdxl", //'flux'
-    //           aspectRatio: "1:1", //Applicable to Flux model only
-    //         },
-    //         {
-    //           headers: {
-    //             "x-api-key": process.env.NEXT_PUBLIC_AIGURULAB_API_KEY,
-    //             "Content-Type": "application/json", // Content Type
-    //           },
-    //         }
-    //       );
-    //       console.log(result.data.image); //Output Result: Base 64 Image
-    //       return result.data.image;
-    //     })
-    //   );
-    //   return images;
-    // });
+    const GenerateImage = await step.run("generateImage", async () => {
+      let images = [];
+      images = await Promise.all(
+        GenerateImagePrompt.map(async (element) => {
+          const result = await axios.post(
+            BASE_URL + "/api/generate-image",
+            {
+              width: 1024,
+              height: 1024,
+              input: element.imagePrompt,
+              model: "sdxl", //'flux'
+              aspectRatio: "1:1",
+            },
+            {
+              headers: {
+                "x-api-key": process.env.NEXT_PUBLIC_AIGURULAB_API_KEY,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          console.error(result.data.image);
+          return result.data.image;
+        })
+      );
+      return images;
+    });
     // save all data to database
     const UpdateVideoDataBD = await step.run("SaveVideoDataToBD", async () => {
       const result = await convex.mutation(api.videoData.UpdateVideoRecord, {
         recordId: recordId,
-        audioUrl: "",
-        images: [],
-        captionJson: [],
+        audioUrl: GenerateAudioFile,
+        images: GenerateImage,
+        captionJson: GenerateCaption,
       });
       return result;
     });
 
-    return UpdateVideoDataBD;
+    return "Executed Successfully";
   }
 );
