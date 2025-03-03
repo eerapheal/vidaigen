66b0e92b554d92206c8179a1e3976fe5e8c8a596
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-03-04 03:48:57 +0530

    get user video by id to play

--- src/app/(main)/play-video/[videoId]/page.jsx
+++ src/app/(main)/play-video/[videoId]/page.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import VideoInfo from '../_component/VideoInfo';
 import RemotionPlay from '../_component/RemotionPlay';
+import { useConvex } from 'convex/react';
+import { useParams } from 'next/navigation';
+import { api } from '../../../../../convex/_generated/api';
 
 const PlayVideo = () => {
+
+  const convex = useConvex();
+  const params = useParams();
+  const videoId = params.videoId;
+  const [videoData, setVideoData] = useState();
+
+  useEffect(() => {
+    videoId && GetVideoDataById();
+  }, [videoId]);
+
+  const GetVideoDataById = async () => {
+    const result = await convex.query(api.videoData.GetUserVideoById, {
+      videoId: videoId
+    })
+    console.log(result)
+    setVideoData(result)
+  };
+
+
+
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10">
       <div>
-<RemotionPlay />
+        <RemotionPlay videoData={videoData} />
       </div>
       <div>
-<VideoInfo />
+        <VideoInfo />
       </div>
     </div>
   )
