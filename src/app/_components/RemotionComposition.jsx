f4c10888ccc37acb8a5e0a6d33ff143aed97477c
Ekpenisi Erue Raphael <Ekpenisiraphael@gmail.com>
2025-03-04 09:57:10 +0530

    sequence image add

--- src/app/_components/RemotionComposition.jsx
+++ src/app/_components/RemotionComposition.jsx
@@ -1,10 +1,12 @@
-'use client'
-import { useVideoConfig} from 'remotion'
-import { useEffect } from 'react'
-const RemotionComposition = ({ videoData, setDurationInFrame }) => {
+'use client';
+import { useEffect } from 'react';
+import { AbsoluteFill, Img, useCurrentFrame, useVideoConfig, Sequence } from 'remotion';
+const RemotionComposition = ({ videoData, setDurationInFrame, DurationInFrame }) => {
 
   const captions = videoData?.captionJson;
   const { fps } = useVideoConfig();
+  const imageList = videoData?.images;
+  const frame = useCurrentFrame();
 
   useEffect(() => {
     videoData && GetDurationFrame();
@@ -13,11 +15,32 @@ const RemotionComposition = ({ videoData, setDurationInFrame }) => {
   const GetDurationFrame = () => {
     const totalDuration = captions[captions?.length - 1]?.end * fps
     setDurationInFrame(totalDuration);
+    return totalDuration;
   }
 
   return (
     <div>
-      RemotionComposition
+      <AbsoluteFill>
+        {imageList?.map((items, index) => {
+          const startTime = (index * GetDurationFrame()) / imageList?.length;
+          const duration = GetDurationFrame();
+          return (
+            <>
+              <Sequence key={items?.index} from={startTime} DurationInFrame={setDurationInFrame()}>
+                <AbsoluteFill>
+                  <Img src={items}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover'
+                    }}
+                  />
+                </AbsoluteFill>
+              </Sequence>
+            </>
+          )
+        })}
+      </AbsoluteFill>
     </div>
   )
 }
